7507c38492d25082fc9c8e3cf9590c696271f6c7
Jonathan Brooks <jonny.brooks@overloop.io>
2024-03-05 16:58:26 +0000

    Auto-launch tmux

---
+++ cli.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+source .env
+SESSION_ID="orchestrator_$(openssl rand -hex 8)"
+PATH_TO_SESSION_FILE="/tmp/$SESSION_ID.txt"
+
+./cli.js $PATH_TO_SESSION_FILE
+SESSION_NAME=$(cat $PATH_TO_SESSION_FILE)
+rm $PATH_TO_SESSION_FILE
+tmux attach -t $SESSION_NAME
--- src/tmux.js
+++ src/tmux.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
 const { randomBytes } = require('crypto');
+const { writeFileSync } = require('fs');
 const config = require('./config');
 
 function sleep(ms) {
@@ -8,11 +9,12 @@ function sleep(ms) {
 
 async function exec(cmd, delay = 0) {
     await sleep(delay);
-    console.log(cmd);
     execSync(cmd);
 }
 
 async function runServices(services) {
+    const PATH_TO_SESSION_FILE = process.argv[2];
+    
     // Create a new session
     const SESSION_NAME = config.randomiseSessionName ?
         randomBytes(8).toString('hex') :
@@ -26,13 +28,16 @@ async function runServices(services) {
         const env = Object.entries(service.env ?? {}).map(([k, v]) => `-e ${k}=${v}`).join(' ');
         const commands = service.commands.join(' && ');
         const cmd = `tmux neww -d -t ${SESSION_NAME}: -n ${service.label} -c ${service.cwd} ${env} "${commands} || zsh"`;
+
+        const delay_message = (service.delay && service.delay > 0) ? ` (with ${service.delay / 1000}s delay)` : '';
+        process.stderr.write(`Launching ${service.label}${delay_message}...\n`);
         await exec(cmd, service.delay);
     }
     
     // Finalise
+    process.stderr.write('Done launching services. Attaching to tmux...\n');
     await exec(`tmux killw -t ${SESSION_NAME}:0`);
-    process.stderr.write(`\nNow run the following to attach to the session:\n`);
-    process.stderr.write(`tmux attach -t ${SESSION_NAME}\n\n`);
+    writeFileSync(PATH_TO_SESSION_FILE, `${SESSION_NAME}\n`);
 }
 
 //
