ecfd0fddfeaeaacffb468eb0cb190270d17da6cb
Jonathan Brooks <jonny.brooks@overloop.io>
2024-03-27 10:57:24 +0000

    Some jsdoc

--- src/hydrateService.ts
+++ src/hydrateService.ts
@@ -1,11 +1,18 @@
 import { Context, ServiceGroupConfig, Service } from "./types";
 import * as utils from './utils';
 
+/**
+* Hydrates (enriches) a service definition with additional metadata using example settings.
+*
+* Here you may define custom logic for defining the service definitions fields. This is useful when you want to
+* define a service's fields based on other service metadata only known at runtime.
+* @param {Context} context is an empty object passed from outside which allows you to attach custom data to each invocation of this function.
+* Keeping track of an incrementing port number, for example, is a good use case for this object.
+* @param {ServiceGroupConfig} group is this service's group config, if it exists.
+* @param {Service} service is the service's base definition, created by `defineBaseService`.
+* @param {Service[]} serviceDefs is a list of all service base definitions, which get hydrated as we go.
+* @return {void} void.
+*/
 export default function(context: Context, group: ServiceGroupConfig, service: Service, serviceDefs: Service[]) {
-    // `context` is an empty object passed from outside which allows you to attach custom data to each invocation of this function.
-    // Keeping track of an incrementing port number, for example, is a good use case for this object.
-    // `group` is this service's group config, if it exists.
-    // `service` is the service's base definition, created by `utils.defineBaseService`.
-    // `serviceDefs` is a list of all service base definitions, which get hydrated as we go.
     return utils.defaultHydrateService(context, group, service, serviceDefs);
 };
--- src/utils.ts
+++ src/utils.ts
@@ -50,13 +50,16 @@ export function defineBaseService(context: Context, group: ServiceGroupConfig, s
     };
 }
 
+/**
+* Hydrates (enriches) a service definition with additional metadata using example settings.
+* @param {Context} context is an empty object passed from outside which allows you to attach custom data to each invocation of this function.
+* Keeping track of an incrementing port number, for example, is a good use case for this object.
+* @param {ServiceGroupConfig} group is this service's group config, if it exists.
+* @param {Service} service is the service's base definition, created by `defineBaseService`.
+* @param {Service[]} serviceDefs is a list of all service base definitions, which get hydrated as we go.
+* @return {void} void.
+*/
 export function defaultHydrateService(context: Context, group: ServiceGroupConfig, service: Service, serviceDefs: Service[]) {
-    // `context` is an empty object passed from outside which allows you to attach custom data to each invocation of this function.
-    // Keeping track of an incrementing port number, for example, is a good use case for this object.
-    // `group` is this service's group config, if it exists.
-    // `service` is the service's base definition, created by `defineBaseService`.
-    // `serviceDefs` is a list of all service base definitions, which get hydrated as we go.
-    
     if(!context.nextPort) context.nextPort = config.baseServicePort;
     
     switch(service.group) {
